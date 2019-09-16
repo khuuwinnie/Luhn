446c78b8bcccd5a9ea7a377421930edced656e10
Winnie K <52090385+khuuwinnie@users.noreply.github.com>
2019-09-16 08:01:04 -0400

    Refactored

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,25 +1,15 @@
 module Luhn
     def self.is_valid?(number)
         array = []
-
         digits = number.to_s.split("")
-
         digits.reverse.each_with_index do |n, i|
-            array << n.to_i unless (i % 2 == 1)
+            n = n.to_i
+            array << n unless (i % 2 == 1)
             if (i % 2 == 1)
-                element = n.to_i * 2
+                element = n * 2
                 array << element unless element > 9 
             end 
-
-            if element.to_i > 9
-                array << (element-9)
-            end
+            array << (element-9) if element.to_i > 9
         end
-        
-        if array.sum % 10 == 0
-            return true
-        else
-            return false
-        end
-    end
+    array.sum % 10 == 0        
 end
